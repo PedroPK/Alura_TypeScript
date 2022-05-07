41cbc0a1a7d6d0ded3ff735f7fa446bb7d05a1f2
PedroPK <pedropk@gmail.com>
2022-05-06 23:31:30 -0300

    Adding a class to Store the Negociations
    Cloning the List of Negociations with [...] (Spread) operator

--- app/models/listaNegociacoes.ts
+++ app/models/listaNegociacoes.ts
@@ -2,4 +2,14 @@ import { Negociacao } from "./negociacao.js";
 
 export class ListaNegociacoes{
     private aNegociacoes: Array<Negociacao> = [];
-}c
+
+    adiciona(negociacao: Negociacao): void  {
+        this.aNegociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao> {
+                // Spread Operator - Makes a Clone of the original Array
+        return [...this.aNegociacoes];
+    }
+
+}
