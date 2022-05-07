0c580c790663679bab66ebc433fd98cb780bdfc7
PedroPK <pedropk@gmail.com>
2022-05-07 07:51:02 -0300

    Simplified usage of Arrays declaration

--- app/models/listaNegociacoes.ts
+++ app/models/listaNegociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class ListaNegociacoes{
-    private aNegociacoes: Array<Negociacao> = [];
+    private aNegociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao): void  {
         this.aNegociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): readonly Negociacao[] {
                 // Spread Operator - Makes a Clone of the original Array
         return this.aNegociacoes;
     }
