45b51d9ed8fce3684d481e800a177f86f2c3e205
PedroPK <pedropk@gmail.com>
2022-05-09 16:12:04 -0300

    Using optional parameter to remove Escape tags

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,16 +1,36 @@
 export abstract class View<T> {
 
-	protected aElement:   HTMLElement;
+	protected	aElement:   HTMLElement;
+	private		aEscape:	boolean		=	false;
 
-	constructor(pSelector: string) {
+	constructor(pSelector: string, pEscape?: boolean) {
 		this.aElement   =   document.querySelector(pSelector);
+
+		if ( pEscape ) {
+			this.aEscape	=	pEscape;
+		}
 	}
 
 	protected abstract template( pMessage: T ): string;
 
 	public update( pMessage: T ): void {
-		const template = this.template( pMessage );
+		let template = this.template( pMessage );
+
+		if ( this.aEscape ) {
+			template = this.removeScriptTag(template);
+		}
+		
 		this.aElement.innerHTML = template;
+	}
+
+	private removeScriptTag(pTemplate: string): string {
+		const result =
+			pTemplate.replace(
+				/<script>[\s\S]*?<\/script>/, 
+				''
+			);
+		
+		return result;
 	}
 
 }
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,9 +1,20 @@
 export class View {
-    constructor(pSelector) {
+    constructor(pSelector, pEscape) {
+        this.aEscape = false;
         this.aElement = document.querySelector(pSelector);
+        if (pEscape) {
+            this.aEscape = pEscape;
+        }
     }
     update(pMessage) {
-        const template = this.template(pMessage);
+        let template = this.template(pMessage);
+        if (this.aEscape) {
+            template = this.removeScriptTag(template);
+        }
         this.aElement.innerHTML = template;
+    }
+    removeScriptTag(pTemplate) {
+        const result = pTemplate.replace(/<script>[\s\S]*?<\/script>/, '');
+        return result;
     }
 }
