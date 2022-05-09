a830a8bb5d66ae4e941d1eef73fbe5089433affa
PedroPK <pedropk@gmail.com>
2022-05-09 12:41:22 -0300

    Early Return and Workday method extracted

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -16,29 +16,47 @@ export class NegociacaoController {
     adiciona() {
         // Capture fields from View and creates a new Negociation
         const negociacao = this.criarNegociacao();
-        /* Workday Rule
-         *	Negociations are done only from Mondays to Fridays
+        /*  Workday Rule
+         *	 - Negociations are done only from Mondays to Fridays
          *
          *	The getDay() method from Date returns a Number to represent the Day of Week
          *	It starts in 0 for Sunday and goes to 6 for Saturday
          *	So, we should accept only values from 1 to 5, excluding 0 and 6
          */
-        if (negociacao.data.getDay() > 0 &&
-            negociacao.data.getDay() < 6) {
-            // Clean up the View's Form from previous data
-            this.limparFormulario();
-            // Inserts the new Negociation in the List
-            this.aListaNegociacoes.adiciona(negociacao);
-            // Updates the View with new Negociation added
-            this.updateView();
-            this.aSucessfullAdded = true;
-            console.log(negociacao);
-            console.log(this.aListaNegociacoes.lista());
-        }
-        else {
+        if (!this.isWorkday(negociacao.data)) {
             this.aSucessfullAdded = false;
-            this.aMessageView.update("Negociações são aceitas apenas em dias úteis!");
+            const message = "Negociações são aceitas apenas em dias úteis!";
+            this.aMessageView.update(message);
+            return; // Early return
         }
+        // Clean up the View's Form from previous data
+        this.limparFormulario();
+        // Inserts the new Negociation in the List
+        this.aListaNegociacoes.adiciona(negociacao);
+        this.aSucessfullAdded = true;
+        // Updates the View with new Negociation added
+        this.updateView();
+    }
+    /**
+     * Evaluates if the Date is from Monday to Friday
+     *
+     * Workday Rule
+     *	 - Negociations are done only from Mondays to Fridays
+     *
+     *	The getDay() method from Date returns a Number to represent the Day of Week
+     *	It starts in 0 for Sunday and goes to 6 for Saturday
+     *	So, we should accept only values from 1 to 5, excluding 0 and 6
+     *
+     * @param	pDate
+     *
+     * @returns	boolean
+     */
+    isWorkday(pDate) {
+        const SUNDAY = 0;
+        const SATURDAY = 6;
+        const result = pDate.getDay() > SUNDAY &&
+            pDate.getDay() < SATURDAY;
+        return result;
     }
     criarNegociacao() {
         const date = this.converterData(this.aInputData.value);
@@ -65,8 +83,9 @@ export class NegociacaoController {
         // Updates the View with the new Negociation
         this.aNegociacoesView.update(this.aListaNegociacoes);
         if (this.aSucessfullAdded) {
+            const message = "Negociação adicionada com sucesso!";
             // Shows a success message to the User
-            this.aMessageView.update("Negociação adicionada com sucesso!");
+            this.aMessageView.update(message);
         }
     }
 }
